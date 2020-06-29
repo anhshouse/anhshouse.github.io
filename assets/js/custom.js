1270112f5eebff0de7d7d53f6cae0752323cc599
anhshouse <67420964+anhshouse@users.noreply.github.com>
2020-06-30 02:40:00 +0700

    update assets

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,7 +5,7 @@ $('.owl-carousel').owlCarousel({
 	margin:10,
 	loop:true,
 	autoplay:true,
-	autoplayTimeout:6000,
+	autoplayTimeout:8000,
 });
 
 ! function($) {
@@ -30,6 +30,36 @@ $('.owl-carousel').owlCarousel({
     });
 
 }(window.jQuery);
+
+
+//SMOOTH SCROLL
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+
+
 
 
 
